8b3e5102483fc6adbe32ced5c81aa819a0643637
hsm207 <hsm207@users.noreply.github.com>
2023-12-15 10:14:12 +0000

    reformat code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,19 @@
-import { KitBuilder } from "@google-labs/breadboard/kits";
-import { index } from "./lib/index.js";
-import { query } from "./lib/query.js";
-
+import { KitBuilder } from '@google-labs/breadboard/kits';
+import { index } from './lib/index.js';
+import { query } from './lib/query.js';
 
 const builder = new KitBuilder({
-    url: "npm:weaviate-kit",
-    title: "Weaviate Kit",
-    description: "A collection of Breadboard nodes for working with Weaviate",
-    version: "0.0.1",
+  url: 'npm:weaviate-kit',
+  title: 'Weaviate Kit',
+  description: 'A collection of Breadboard nodes for working with Weaviate',
+  version: '0.0.1',
 });
-
 
 const WeaviateKit = builder.build({
-    // node handlers go here
-    index,
-    query,
+  // node handlers go here
+  index,
+  query,
 });
-
 
 // Necessary for Breadboard to import it as a Kit when loading boards
 export default WeaviateKit;
@@ -24,4 +21,3 @@ export default WeaviateKit;
 export type WeaviateKit = InstanceType<typeof WeaviateKit>;
 // Optionally, export it as a named export
 export { WeaviateKit };
-
--- src/lib/weaviate-client.ts
+++ src/lib/weaviate-client.ts
@@ -2,7 +2,7 @@ import weaviate, { ApiKey, WeaviateClient } from 'weaviate-ts-client';
 
 /**
  * Creates a Weaviate client with the specified Weaviate URL and optional API key.
- * 
+ *
  * @param weaviateUrl The URL of the Weaviate instance to connect to.
  * @param palmApiKey The API key to authenticate into the Google PaLM API service.
  * @param weaviateApiKey The API key to authenticate into the weaviate instance (optional).
@@ -10,27 +10,31 @@ import weaviate, { ApiKey, WeaviateClient } from 'weaviate-ts-client';
  *
  * @remarks If the protocol is not specified in the `weaviateUrl`, it defaults to `http`.
  */
-export function createWeaviateClient(weaviateUrl: string, palmApiKey: string, weaviateApiKey?: string): WeaviateClient {
-    if (!weaviateUrl.startsWith('http://') && !weaviateUrl.startsWith('https://')) {
-        weaviateUrl = 'http://' + weaviateUrl;
-    }
+export function createWeaviateClient(
+  weaviateUrl: string,
+  palmApiKey: string,
+  weaviateApiKey?: string
+): WeaviateClient {
+  if (!weaviateUrl.startsWith('http://') && !weaviateUrl.startsWith('https://')) {
+    weaviateUrl = 'http://' + weaviateUrl;
+  }
 
-    const parsedUrl = new URL(weaviateUrl);
-    const scheme = parsedUrl.protocol.replace(':', '');
-    const host = parsedUrl.host;
+  const parsedUrl = new URL(weaviateUrl);
+  const scheme = parsedUrl.protocol.replace(':', '');
+  const host = parsedUrl.host;
 
-    if (weaviateApiKey) {
-        return weaviate.client({
-            scheme: scheme,
-            host: host,
-            apiKey: new ApiKey(weaviateApiKey),
-            headers: { "X-Palm-Api-Key": palmApiKey}
-        });
-    } else {
-        return weaviate.client({
-            scheme: scheme,
-            host: host,
-            headers: { "X-Palm-Api-Key": palmApiKey}
-        });
-    }
+  if (weaviateApiKey) {
+    return weaviate.client({
+      scheme: scheme,
+      host: host,
+      apiKey: new ApiKey(weaviateApiKey),
+      headers: { 'X-Palm-Api-Key': palmApiKey },
+    });
+  } else {
+    return weaviate.client({
+      scheme: scheme,
+      host: host,
+      headers: { 'X-Palm-Api-Key': palmApiKey },
+    });
+  }
 }
