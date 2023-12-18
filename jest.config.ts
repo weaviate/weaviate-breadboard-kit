e27b6b67974474c8dc7d8d5af64a510a1fa92958
Joseph Mearman <joseph@mearman.co.uk>
2023-12-18 15:02:47 +0000

    Update Jest testMatch configuration
    - Updated the testMatch configuration in jest.config.ts to include the pattern "**/?(*.)+(spec|test).[tj]s?(x)" for detecting test files.
    - Removed the commented out testMatch patterns.

--- jest.config.ts
+++ jest.config.ts
@@ -146,10 +146,10 @@ const config: JestConfigWithTsJest = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
+  testMatch: [
   //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
