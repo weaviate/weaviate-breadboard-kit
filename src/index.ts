2982da91ee8c577018c895375ca76f64197efd57
Joseph Mearman <joseph@mearman.co.uk>
2023-12-18 16:12:03 +0000

    Move type declaration for WeaviateKit

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const WeaviateKit = builder.build({
   query,
 });
 
+// Necessary for TypeScript to recognize the type of the Kit
+type WeaviateKit = InstanceType<typeof WeaviateKit>;
 // Necessary for Breadboard to import it as a Kit when loading boards
 export default WeaviateKit;
-// Necessary for TypeScript to recognize the type of the Kit
-export type WeaviateKit = InstanceType<typeof WeaviateKit>;
